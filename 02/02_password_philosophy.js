3bbe2e1c11b2478acd966181f90161c185fc9cca
Andy Stanford-Bluntish <andy@bluntish.net>
2021-11-23 16:58:00 +1030

    2020/02 2

---
+++ 02/02_password_philosophy.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const { join } = require("path");
+const { readFile } = require("fs");
+const { EOL } = require("os");
+
+const filePath = join(__dirname, "input.txt");
+
+readFile(filePath, "utf8", (err, data) => {
+  if (err) throw err;
+  const lines = data.split(EOL);
+  const valid = lines
+    .map((line) => {
+      if (!line.trim().length) return false;
+
+      const [, p1, p2, char, password] =
+        line.match(/(\d+)-(\d+)\s?(\w):\s?(.*)/i) ?? [];
+      const char1 = password.charAt(parseInt(p1, 10) - 1);
+      const char2 = password.charAt(parseInt(p2, 10) - 1);
+
+      return char1 !== char2 && [char1, char2].includes(char);
+    })
+    .filter(Boolean);
+
+  console.log("Result:");
+  console.log(`out of ${lines.length} passwords`);
+  console.log(`${valid.length} are valid`);
+  console.log(`and ${lines.length - valid.length} are invalid.`);
+});
