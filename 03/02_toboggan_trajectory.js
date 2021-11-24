d1cd4e06377dce794e9746b54ce5d7c239a8922d
Andy Stanford-Bluntish <andy@bluntish.net>
2021-11-24 22:09:07 +1030

    2020/03 2

---
+++ 03/02_toboggan_trajectory.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+const { join } = require("path");
+const { readFile } = require("fs");
+const { EOL } = require("os");
+const filePath = join(__dirname, "input.txt");
+
+function traverse(map, xDistance, yDistance) {
+  const width = map[0].length;
+  const height = map.length;
+
+  let x = 0;
+  let y = 0;
+  let trees = 0;
+
+  while (y < height) {
+    const location = map[y][x];
+
+    if (location === "#") {
+      trees++;
+    }
+
+    x = (x + xDistance) % width;
+    y += yDistance;
+  }
+
+  return trees;
+}
+
+readFile(filePath, "utf8", (err, data) => {
+  if (err) throw err;
+  const lines = data.split(EOL);
+  const attempts = [
+    { right: 1, down: 1 },
+    { right: 3, down: 1 },
+    { right: 5, down: 1 },
+    { right: 7, down: 1 },
+    { right: 1, down: 2 },
+  ];
+
+  const result = attempts
+    .map(({ right, down }) => {
+      return traverse(lines, right, down);
+    })
+    .reduce((prev, curr) => {
+      return prev * curr;
+    }, 1);
+
+  console.log("Result:");
+  console.log(
+    `The product of the sum of trees in all the attempts is ${result}.`
+  );
+});
